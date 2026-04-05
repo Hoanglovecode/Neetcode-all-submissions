0364e3ce6d57fa871dc61c691d5b8957f5165cc4
Lê Văn Hoàng(Math) <Van.Hoang.10.05.2007@gmail.com>
2026-04-05 23:14:53 +0700

    Add: merge-k-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-1.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        vector<int>nodes;
+        for(ListNode*lit:lists){
+            while(lit){
+                nodes.push_back(lit->val);
+                lit=lit->next;
+            }
+        }
+        sort(nodes.begin(),nodes.end());
+        ListNode dummy(0);
+        ListNode* tmp=&dummy;
+        for(int x: nodes){
+            tmp->next=new ListNode(x);
+            tmp=tmp->next;
+        }
+        return dummy.next;
+    }
+};
